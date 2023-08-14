2506b375272b7b0053ad69427c6308ad8056a07c
Vitor Daniel <vitor036daniel@gmail.com>
2023-08-13 21:06:15 -0300

    fix

--- src/pages/botoes/index.tsx
+++ src/pages/botoes/index.tsx
@@ -19,6 +19,7 @@ export default function() {
 
   return (
     <div class="h-screen w-full flex flex-col gap-5 items-center justify-center">
+      <p>Como o Workana não permite valores menores. Se você quiser, pode <a class="link" href="http://mpago.la/27aocLm">pagar por aqui</a> e <a class="link" href="https://api.whatsapp.com/send/?phone=%2B5584981437676&text&type=phone_number&app_absent=0">me chamar no Whatsapp</a>.</p>
       <div class="form-control w-full max-w-xs">
         <label class="label">
           <span class="label-text">Número da sala</span>
